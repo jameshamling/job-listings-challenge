b47ef4010d77d685c73bb3a934553c75b9e6d7b5
jameshamling <93711665+jameshamling@users.noreply.github.com>
2024-05-18 12:14:30 +0100

    add start up files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 style={{fontFamily: 'sans-serif', color: '#337'}}>Job Listings Challenge ☺</h1>
+    </>
+  )
+}
+
+export default App
